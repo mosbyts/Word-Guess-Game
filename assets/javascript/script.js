3a96457661f42df2a593593f5af04c659b9420c4
Tianna Mosby <mosbyts@vcu.edu>
2019-11-10 09:07:56 -0500

    Updated CSS, added footer to HTML, included user input validation and cleaned up code.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,56 +6,48 @@ $(document).ready(function(){
     var halloweenWords = ["The Nightmare Before Christmas", "Hocus Pocus", "Pet Sematary", "The Ring", "Paranormal Activity", "Sleepy Hollow", "A Nightmare On Elm Street", "Halloween"]; 
     var wordBlanks = [];
     var chosenWordLetters;
+
 //This function will pick a random word for the user to guess, display the blanks and update/reset values.
     function setUpGame(){
-//Word is chosen at random then stored in chosenWord variable.
-        var x = Math.floor(Math.random() * halloweenWords.length);
-        var chosenWord = halloweenWords[x];
+        var chosenWord = halloweenWords[Math.floor(Math.random() * halloweenWords.length)];
         console.log(chosenWord);
 //Put the letters in chosenWord as array.
         chosenWordLetters = chosenWord.split("");
 //Loop through the amount of letters in the chosen word and display a blank for each.
+        wordBlanks = [];
         for(var i = 0; i < chosenWordLetters.length; i++){
             wordBlanks.push("_ ");
-        };
-        
+        };       
         $("#wordBlanks").text(wordBlanks.join(""));
 //Update wins, guesses left and empty lettersGuessed div.
         $("#wins").text(wins);
+        guessesLeft = 10;
         $("#guessesLeft").text(guessesLeft);
+        lettersGuessed = [];
         $("#lettersGuessed").empty();
     };
-
+//Run the function when page loads
     setUpGame();
 
 //When user presses a key, guesses left decreases and the letter is added to letters guessed.
-        $(document).on("keyup", function(event){
+    $(document).on("keyup", function(event){
+        if(event.key >= "a" && event.key <= "z"){
             lettersGuessed.push(event.key);
             guessesLeft--;
             $("#guessesLeft").text(guessesLeft);
             $("#lettersGuessed").text(lettersGuessed);
-
-            for(var x = 0; x < chosenWordLetters.length; x++){
-                if(event.key == chosenWordLetters[x]){
-                    console.log("It matches!");
-                    wordBlanks[x] = event.key;
-                };
+        for(var x = 0; x < chosenWordLetters.length; x++){
+            if(event.key == chosenWordLetters[x]){
+                wordBlanks[x] = event.key;
             };
-            console.log(wordBlanks);
-            $("#wordBlanks").text(wordBlanks.join(""));
+        };
+        }else(alert("Only letters are allowed, please try again."));
 
-            if(guessesLeft == 0){
-                resetGame();
-                setUpGame();
-            };
-        });
+        $("#wordBlanks").text(wordBlanks.join(""));
 
-//When the game ends, the gueses left will reset, the letters guessed will empty and the wins will update.
-    function resetGame(){
-        wordBlanks = [];
-        guessesLeft = 10;
-        $("#guessesLeft").text(guessesLeft);
-        $("#lettersGuessed").empty();
-        $("#wins").text(wins);
-    };
+        if(guessesLeft == 0){
+            alert("You lost, try again!");
+            setUpGame();
+        };
+    });
 });
